93d6f4600ce098130394abbddfa9ae1c340c3da4
sh1vrm <sanketh.mekala@edu.sait.ca>
2024-03-30 22:56:49 -0600

    Final Changes

--- App.js
+++ App.js
@@ -1,20 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import ToDoList from './ToDoList'; 
 
-export default function App() {
+function App() {
+  const [tasks, setTasks] = useState([
+    'Do laundry',
+    'Go to gym',
+    'Walk dog'
+  ]);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <div>
+      <ToDoList tasks={tasks} />
+    </div>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ ToDoList.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function ToDoList({ tasks }) {
+  return (
+    <ul>
+      {tasks.map((task, index) => (
+        <li key={index}>{task}</li>
+      ))}
+    </ul>
+  );
+}
+
+export default ToDoList;
